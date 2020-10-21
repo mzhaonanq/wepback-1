5a4df881821f7c9af12d20270af629267632417c
mzhaonanq <1002604003@qq.com>
2020-10-21 20:11:55 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
   },
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({ title: "huaQ",template:"./src/index.html"}),
   ],
